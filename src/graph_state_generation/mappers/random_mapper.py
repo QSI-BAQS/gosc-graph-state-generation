171a0c6e681aad24dd2a63a6577314ba29d06f38
Alan Robertson <alan@qrop.xyz>
2024-09-26 00:35:43 +1000

    Updated random mapper

--- src/graph_state_generation/mappers/random_mapper.py
+++ src/graph_state_generation/mappers/random_mapper.py
@@ -15,13 +15,5 @@ class RandomMapper(mapper.Mapper):
         mapper_fn
         Random Map
         '''
-        flag = ctypes.c_int32(-1)
-        ctypes.memset(self.map, flag, 4 * self.n_elements)
-        n_set = 0
-        flag = int.from_bytes(flag, signed=True)
-        # Not a good implementation,
-        while n_set < self.n_elements:
-            idx = rand(0, self.n_elements - 1)
-            if self[idx] == flag:
-                self[idx] = n_set
-                n_set += 1
+        self.map = list(map(ctypes.cint32, random.sample(range(self.n_elements), self.n_elements)))
+
