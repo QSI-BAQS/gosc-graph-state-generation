33df25d6d54d0fe6267f5db6891c379425b76216
Alan Robertson <alan@qrop.xyz>
2024-09-26 00:51:27 +1000

    Linting scheduler

--- src/graph_state_generation/schedulers/greedy_stabiliser_measurement_scheduler.py
+++ src/graph_state_generation/schedulers/greedy_stabiliser_measurement_scheduler.py
@@ -1,20 +1,22 @@
+'''
+    Constructs a greedy schedule
+'''
 import bisect
-
-from graph_state_generation.graph_state import graph_state 
-from graph_state_generation.schedulers import scheduler 
+from graph_state_generation.schedulers import scheduler
 
 class GreedyStabiliserMeasurementSchedulerLeft(scheduler.Scheduler):
-
+    '''
+        Constructs a greedy schedule
+    '''
     left = 0
     right = 1
 
-
-    def greedy_schedule(self, *args, **kwargs): 
+    def greedy_schedule(self, *args, **kwargs):
         '''
             Constructs a greedy schedule
         '''
 
-        lr_segments = list(zip(map(self.get_lr, self.graph), self.graph)) 
+        lr_segments = list(zip(map(self.get_lr, self.graph), self.graph))
         lr_segments.sort(key=lambda x: x[0][self.left]) # Gets the self.left value
 
         curr_right = 0
@@ -22,28 +24,29 @@ class GreedyStabiliserMeasurementSchedulerLeft(scheduler.Scheduler):
 
         self.schedule_layers.append([])
 
-        while len(lr_segments) > 0: 
-    
+        while len(lr_segments) > 0:
             # Reached end of layer
-            if lr_segments[-1][0][self.left] < curr_right:  
-                curr_layer += 1 
+            if lr_segments[-1][0][self.left] < curr_right:
+                curr_layer += 1
                 curr_right = 0
                 self.schedule_layers.append([])
                 continue
 
-            # Extract element 
-            next_segment_idx = bisect.bisect_left(lr_segments, curr_right, key=lambda x: x[0][self.left]) 
-            lr, graph_node = lr_segments.pop(next_segment_idx) 
+            # Extract element
+            next_segment_idx = bisect.bisect_left(
+                lr_segments,
+                curr_right,
+                key=lambda x: x[0][self.left])
+            lr, graph_node = lr_segments.pop(next_segment_idx)
             self.schedule_layers[-1].append(graph_node)
             curr_right = lr[self.right] + 1
 
     def __call__(self, *args, **kwargs):
         return self.greedy_schedule(*args, **kwargs)
 
-
-    def get_lr(self, graph_node): 
+    def get_lr(self, graph_node):
         '''
-           Maps a node to its leftmost and right most elements 
+           Maps a node to its leftmost and right most elements
         '''
         mapped_node = self.apply_mapper(graph_node)
         return (min(mapped_node), max(mapped_node))
--- src/graph_state_generation/schedulers/scheduler.py
+++ src/graph_state_generation/schedulers/scheduler.py
@@ -1,18 +1,28 @@
+'''
+    Scheduler base object
+'''
 import abc
 import bisect
 
 class Scheduler(abc.ABC):
+    '''
+        Scheduler base object
+    '''
     def __init__(self, graph, mapper):
+        '''
+            scheduler base object
+        '''
         self.graph = graph
         self.mapper = mapper
         self.schedule_layers = []
 
-
-    def apply_mapper(self, graph_node):
+    def apply_mapper(self, graph_node) -> list:
+        '''
+            Applies the mapper to a graph node
+        '''
         mapped_graph_node = [self.mapper[i] for i in graph_node]
         bisect.insort(mapped_graph_node, graph_node.qubit_idx)
         return mapped_graph_node
-        
 
     @abc.abstractmethod
     def __call__(self, *args, **kwargs):
