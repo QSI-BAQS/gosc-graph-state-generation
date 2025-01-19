5454d3e3a10fd3318866c87640d5d8fdb023a195
Alan Robertson <alan@qrop.xyz>
2025-01-20 10:14:46 +1100

    Added weight sort  linear mapper example

--- examples/linear_example.py
+++ examples/linear_example.py
@@ -1,4 +1,4 @@
-from graph_state_generation.mappers import linear_mapper
+from graph_state_generation.mappers import linear_mapper, weight_sort_mapper
 from graph_state_generation.schedulers import  greedy_cz_scheduler
 from graph_state_generation.graph_state import example_graphs
 
@@ -7,4 +7,8 @@ n_vertices = 100
 gs = example_graphs.graph_binary_tree(n_vertices)
 
 mapped_gs = linear_mapper.LinearMapper(gs)
+
+ws_mapped_gs = weight_sort_mapper.LinearWeightSortMapper(gs)
+
+
 schedule = greedy_cz_scheduler.GreedyCZScheduler(gs, mapped_gs) 
